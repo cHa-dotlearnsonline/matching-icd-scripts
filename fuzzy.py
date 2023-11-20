d5a70062f4b8ebcc15b96ad38d07d12902113568
CHAWANANGWA PHIRI <chawalearnsonline@gmail.com>
2023-11-20 08:23:56 +0200

    The code I used to match icd11 to HIA 1 B.
    This code does not match things perfectly but it was good enought to get the job done for me.

---
+++ fuzzy.py
@@ -0,0 +1,56 @@
+from fuzzywuzzy import fuzz 
+from fuzzywuzzy import process
+import openpyxl 
+from openpyxl import Workbook
+
+HIA_data_wb = openpyxl.load_workbook("HIA 1 B diagnosis.xlsx")
+ICD11_data_wb = openpyxl.load_workbook("ICD SC evo.xlsx")
+
+HIA_data_ws = HIA_data_wb["Sheet1"]
+
+ICD11_data_ws = ICD11_data_wb["icd11A"]
+
+# let's load all the data from the excel sheet for HIA into a list
+all_rows = HIA_data_ws.max_row
+column = "A"
+all_HIA_diagnosis = []
+for i in range(2,all_rows):
+    # Chawa Phiri ["Chawa" ,"PHiri"]
+      
+    all_HIA_diagnosis.append(" ".join(str(HIA_data_ws[f"{column}{i}"].value).lower().split()))
+
+# let's load all the diagnosis for ICD11 into a list as well
+all_icd11_rows = ICD11_data_ws.max_row
+all_ICD11_diagnosis = []
+for i in range(1, all_icd11_rows):
+    
+    all_ICD11_diagnosis.append(" ".join(str(ICD11_data_ws[f"B{i}"].value).lower().split()))
+
+new_workbook = Workbook()
+tracker = 1
+for diagnosis in all_HIA_diagnosis:
+    #worksheet = new_workbook.create_sheet(title=str(tracker))
+    #new_workbook.save()
+    # worksheet1 = new_workbook[str(tracker)]
+    worksheet1 = new_workbook["Sheet"]
+    my_boy = process.extract(diagnosis, all_ICD11_diagnosis, limit=50)
+    "(diagnosis, 90)"
+    my_rexo =[]
+    for boy in my_boy:
+        if int(boy[1]) >= 88:
+            my_rexo.append(boy[0])
+    # my_rexo = [ boy for boy in my_rexo if boy[1] == 90]
+    my_rexo = [ str(boy) for boy in my_rexo]
+    worksheet1.append([diagnosis])
+    worksheet1[f"B{tracker}"].value = " + ".join(my_rexo)
+    # for result in my_rexo:
+    #     rezo = list(result)
+    #     worksheet1.append(rezo)
+    tracker += 1
+new_workbook.save("matched_diagnosis_hia_1_b(3).xlsx")
+
+# for result in my_results:
+#     print(result)
+
+HIA_data_wb.close()
+ICD11_data_wb.close()
---
+++ match_oid.py
@@ -0,0 +1,42 @@
+import openpyxl 
+from openpyxl import Workbook
+
+#load all the workbooks I am using today:
+HIA_data_wb = openpyxl.load_workbook("matched_diagnosis_hia1b_final.xlsx")
+ICD11_Oid_data_wb = openpyxl.load_workbook("CAREPRO-ICD11.xlsx")
+
+HIA_data_ws = HIA_data_wb["Sheet1"]
+
+ICD11_Oid_data_ws = ICD11_Oid_data_wb["Sheet1"]
+
+# let's load the data from HIA into a dictionary that matches the name to the code:
+all_hia_rows = HIA_data_ws.max_row 
+A = "A"
+B = "B"
+
+all_oid_rows = ICD11_Oid_data_ws.max_row
+matched_material2 = {}
+for i in range(2, all_oid_rows):
+    matched_material2[" ".join(str(ICD11_Oid_data_ws[f"C{i}"].value).lower().split())] = str(ICD11_Oid_data_ws[f"A{i}"].value).lower()
+
+new_workbook = Workbook()
+new_worksheet = new_workbook.create_sheet(title="Matched to Oid")
+
+new_worksheet = new_workbook["Matched to Oid"]
+total_hia_rows = HIA_data_ws.max_row
+for i in range(1, total_hia_rows):  
+    matcher = HIA_data_ws[f"C{i}"].value
+    new_worksheet.cell(row=i, column=1).value = HIA_data_ws[f"A{i}"].value
+    if matcher is not None:
+        matches = HIA_data_ws[f"C{i}"].value.split("+")
+        matches1 = [" ".join(diagnosis.lower().split()) for diagnosis in matches]
+        my_diagnosis = []
+        for match in matches1:
+            if match in matched_material2.keys():
+                my_diagnosis.append(matched_material2[match])
+        new_worksheet.cell(row=i, column=2).value = ",".join(my_diagnosis)
+        # new_worksheet.append([HIA_data_ws[f"A{i}"].value.lower(), ",".join(my_diagnosis)])  
+
+new_workbook.save("HIA_1_B_Matched_to_Oid(5)_final.xlsx")
+HIA_data_wb.close()
+ICD11_Oid_data_wb.close()
